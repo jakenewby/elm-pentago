2eb4725b81011da3519b33233207cff7b3ce875f
Jake Newby <jacob.newby@inin.com>
2017-10-09 07:12:45 -0400

    add some types, further build out dom

--- src/Main.elm
+++ src/Main.elm
@@ -1,39 +1,97 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div)
-import Html.Attributes exposing (..)
+import Html exposing (Html, button, text, div)
+import Html.Attributes exposing (class)
 
 
-cellsDom : List (Html msg)
-cellsDom =
-    List.map cellDom (List.range 1 9)
+-- import Html.Events exposing (onClick)
+
+import Maybe exposing (..)
 
 
-cellDom : Int -> Html msg
-cellDom id =
-    div [ class (cellClass id) ]
-        [ div [ class "token token--player-1" ] []
-        ]
+cellsDom : List Cell -> List (Html msg)
+cellsDom cells =
+    List.map cellDom cells
 
 
-cellClass : Int -> String
-cellClass id =
-    "cell cell-" ++ (toString id)
+cellDom : Cell -> Html msg
+cellDom cell =
+    let
+        ( sectionId, cellId ) =
+            (.coordinates cell)
+    in
+        div [ class (cellClass cell) ]
+            [ div [ class "token token--player-1" ] []
+            ]
 
 
-sectionsDom : List (Html msg)
-sectionsDom =
-    List.map sectionDom (List.range 1 4)
+cellClass : Cell -> String
+cellClass cell =
+    let
+        ( sectionId, cellId ) =
+            (.coordinates cell)
+    in
+        "cell cell-" ++ (toString cellId)
 
 
-sectionDom : Int -> Html msg
-sectionDom id =
-    div [ class (sectionClass id) ] cellsDom
+sectionsDom : List Section -> List (Html msg)
+sectionsDom sections =
+    [ (div [ class "board__row" ]
+        (List.map
+            sectionDom
+            (List.take 2 sections)
+        )
+      )
+    , (div
+        [ class "board__row" ]
+        (List.map
+            sectionDom
+            (List.drop 2 sections)
+        )
+      )
+    ]
 
 
-sectionClass : Int -> String
-sectionClass id =
-    "section section-" ++ (toString id)
+sectionDom : Section -> Html msg
+sectionDom section =
+    div [ class (sectionClass section) ] (cellsDom (.cells section))
+
+
+sectionClass : Section -> String
+sectionClass section =
+    "section section-" ++ (toString (.id section))
+
+
+initSections : List Section
+initSections =
+    List.map initSection (List.range 1 4)
+
+
+initSection : Int -> Section
+initSection id =
+    { id = id, angle = 0, cells = (initSectionCells id) }
+
+
+initSectionCells : Int -> List Cell
+initSectionCells sectionId =
+    List.map (initSectionCell sectionId) (List.range 1 9)
+
+
+initSectionCell : Int -> Int -> Cell
+initSectionCell sectionId cellId =
+    { coordinates = ( sectionId, cellId )
+    , token = Nothing
+    }
+
+
+updateAngle : Int -> Direction -> Int
+updateAngle angle direction =
+    case direction of
+        Clockwise ->
+            angle + 90
+
+        CounterClockwise ->
+            angle - 90
 
 
 
@@ -41,12 +99,41 @@ sectionClass id =
 
 
 type alias Model =
-    {}
+    { sections : List Section }
+
+
+type alias Section =
+    { id : Int
+    , angle : Int
+    , cells : List Cell
+    }
+
+
+type alias Cell =
+    { coordinates : ( Int, Int )
+    , token : Maybe Token
+    }
+
+
+type alias Token =
+    { player : Player
+    }
+
+
+type alias Player =
+    { id : Int
+    , name : String
+    }
+
+
+type Direction
+    = Clockwise
+    | CounterClockwise
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { sections = initSections }, Cmd.none )
 
 
 
@@ -55,11 +142,25 @@ init =
 
 type Msg
     = NoOp
+    | RotateSection Section Direction
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        RotateSection section direction ->
+            let
+                rotateSection : Section -> Section
+                rotateSection s =
+                    if (.id s) == (.id section) then
+                        { section | angle = updateAngle (.angle section) direction }
+                    else
+                        section
+            in
+                ( { model | sections = List.map rotateSection (.sections model) }, Cmd.none )
 
 
 
@@ -68,7 +169,7 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [ class "board" ] sectionsDom
+    div [ class "board" ] (sectionsDom (.sections model))
 
 
 
